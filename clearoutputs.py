d1fbe9616573ddddfb5bde087737e9eeea52f296
Eric Hunsberger <eric.hunsberger@appliedbrainresearch.com>
2019-02-28 11:42:20 -0500

    Clear trailing whitespace with clearoutputs.py

--- clearoutputs.py
+++ clearoutputs.py
@@ -10,37 +10,54 @@ import nbformat
 from nbformat import write as write_nb
 
 
+def read_nb(nb):
+    return nbformat.read(nb, as_version=4)
+
+
 def remove_outputs(nb):
     """remove the outputs from a notebook"""
 
+    # --- Remove bad metadata
+    # Should pass `nengo/tests/test_examples.py:test_minimal_metadata`
+    if 'kernelspec' in nb.metadata:
+        del nb.metadata['kernelspec']
     if 'signature' in nb.metadata:
         del nb.metadata['signature']
 
-    if 'kernelspec' in nb.metadata:
-        del nb.metadata['kernelspec']
+    language_info = getattr(nb.metadata, 'language_info', {})
+    badinfo = (
+        "codemirror_mode",
+        "file_extension",
+        "mimetype",
+        "nbconvert_exporter",
+        "version",
+    )
+    for badkey in badinfo:
+        if badkey in language_info:
+            del language_info[badkey]
 
-    nb.metadata['language_info'] = {
-        'name': 'python',
-        'pygments_lexer': 'ipython3'
-    }
-
+    # --- Clean up cells
     worksheets = nb.worksheets if hasattr(nb, 'worksheets') else [nb]
     for ws in worksheets:
         empty = []  # empty cells
 
         for cell in ws.cells:
-            source = (getattr(cell, 'input', cell.source)
-                      if cell.cell_type == 'code'
-                      else getattr(cell, 'source', []))
-            if len(source) == 0:
+            source_key = ('input'
+                          if cell.cell_type == 'code' and 'input' in cell
+                          else 'source')
+            source = getattr(cell, source_key, None)
+            if source is not None and len(source) == 0:
                 empty.append(cell)
                 continue
 
             if cell.cell_type == 'code':
+                # remove any output (print statements, plots, etc.)
                 cell.outputs = []
-                if 'prompt_number' in cell:
+
+                # reset cell execution number
+                if 'prompt_number' in cell:  # notebook version < 4
                     del cell['prompt_number']
-                if 'execution_count' in cell:
+                if 'execution_count' in cell:  # notebook version >= 4
                     cell['execution_count'] = None
 
                 # clear non-useful metadata
@@ -53,6 +70,15 @@ def remove_outputs(nb):
                 clear_cell_metadata_entry(cell, 'deletable', value=True)
                 clear_cell_metadata_entry(cell, 'editable', value=True)
 
+            # clear whitespace at ends of lines
+            if source is not None:
+                assert isinstance(source, str)
+                lines = source.split('\n')
+                for k, line in enumerate(lines):
+                    lines[k] = line.rstrip(' ')
+
+                cell[source_key] = '\n'.join(lines)
+
         # remove empty cells
         for cell in empty:
             ws.cells.remove(cell)
@@ -60,21 +86,20 @@ def remove_outputs(nb):
 
 def clear_cell_metadata_entry(cell, key, value='_ANY_'):
     metadata = cell['metadata']
-    if key in metadata and (value is '_ANY_' or metadata[key] == value):
+    if key in metadata and (value == '_ANY_' or metadata[key] == value):
         del metadata[key]
 
 
 def clear_file(fname, target_version):
     with io.open(fname, 'r', encoding='utf-8') as f:
-        nb = nbformat.read(f, as_version=4)
+        nb = read_nb(f)
     remove_outputs(nb)
     with io.open(fname, 'w', encoding='utf-8') as f:
         write_nb(nb, f, version=target_version)
-        # f.write(u'\n')
     print("wrote %s" % fname)
 
 
-def clear_files(fnames, target_version):
+def clear_paths(fnames, target_version):
     for fname in fnames:
         if os.path.isdir(fname):
             clear_directory(fname, target_version=target_version)
@@ -87,7 +112,7 @@ def clear_directory(dname, target_version):
     fnames = os.listdir(dname)
     fpaths = [os.path.join(dname, fname) for fname in fnames
               if not fname.startswith('.')]
-    clear_files([fpath for fpath in fpaths
+    clear_paths([fpath for fpath in fpaths
                  if os.path.isdir(fpath) or fpath.endswith('.ipynb')],
                 target_version=target_version)
 
@@ -106,7 +131,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     if len(args.fnames) > 0:
-        clear_files(args.fnames, target_version=args.target_version)
+        clear_paths(args.fnames, target_version=args.target_version)
     else:
         nb = read_nb(sys.stdin)
         remove_outputs(nb)
